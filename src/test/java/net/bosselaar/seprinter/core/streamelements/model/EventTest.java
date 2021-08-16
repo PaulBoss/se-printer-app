f93c394ac1f0180af8dc1626d7cfd086af6c3288
Paul Bosselaar <paul@bosselaar.net>
2021-08-16 21:08:05 +0200

    Cleanup EventTest, add missing config options to config.yml

--- src/test/java/net/bosselaar/seprinter/core/streamelements/model/EventTest.java
+++ src/test/java/net/bosselaar/seprinter/core/streamelements/model/EventTest.java
@@ -2,6 +2,11 @@ package net.bosselaar.seprinter.core.streamelements.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.bosselaar.seprinter.config.PrinterConfig;
+import net.bosselaar.seprinter.core.printer.DefaultPrinterPrinter;
+import net.bosselaar.seprinter.core.printer.IReceiptPrinter;
+import net.bosselaar.seprinter.core.printer.PrinterListener;
+import net.bosselaar.seprinter.core.streamelements.ISEEventListener;
 import net.bosselaar.seprinter.core.twitch.TwitchApi;
 import org.junit.Test;
 
@@ -59,27 +64,5 @@ public class EventTest {
         assertThat(event.data.displayName).isEqualTo("AnAnonymousCheerer");
         assertThat(event.data.message).isEqualTo("Anon100");
     }
-
-
-
-    // Shortcut to quicktest the actual printing.
-    public static void main(String[] args) throws IOException {
-        TwitchApi ah = new TwitchApi("qsk1wl8hnl2r1uk1agg1xzw7nomync", "30laerf3pngnmf1mp5zmagsuw11xii");
-
-        ah.getUserAvatarUrl("creepy113").ifPresent(e -> System.out.println(e));
-/*
-        PrinterConfig printerConfig = new PrinterConfig();
-        printerConfig.angle = 90;
-
-        IReceiptPrinter printer = new DefaultPrinterPrinter(printerConfig);
-
-        printer.start();
-
-        Event event = MAPPER.readValue(fixture("fixtures/se-gifted-sub.json"), Event.class);
-        printer.addJob(event);
-
-        printer.stop(); */
-    }
-
 
 }
